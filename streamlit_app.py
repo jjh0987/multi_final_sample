bcd86a0fee129016737ec984699e7f51bd3ae201
jjh0987 <djflswlstlf@naver.com>
2022-06-02 15:06:58 +0900

    2022-06-02

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,43 @@ st.experimental_set_query_params(
      TEAM=['AnotherSense'],
      Project=['AntsMIND']
 )
+
+
+
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data():
+    db = client.final_project
+    items = db.naver_score.find()
+    items = list(items)  # make hashable for st.experimental_memo
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['score1']} has a :{item['score2']}:")
+
+
+
+
+
+
+
+
 """
 # Welcome to Streamlit!
 
